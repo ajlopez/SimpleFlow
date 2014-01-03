9b8368c03c609d99809b6211801add740c8beff7
ajlopez <webmaster@ajlopez.com>
2014-01-03 18:02:08 -0300

    Flow run first tests

--- test/flow.js
+++ test/flow.js
@@ -2,5 +2,42 @@
 var simpleflow = require('..');
 
 exports['Create flow'] = function (test) {
-    test.ok(simpleflow.createFlow(null, null));
+    test.ok(simpleflow.createFlow([], function (err) {}));
+};
+
+exports['Run one step flow'] = function (test) {
+    test.async();
+    var flow = simpleflow.createFlow([step1], errfn);
+    
+    flow.run(1);
+    
+    function step1(value) {
+        test.equal(value, 1);
+        test.done();
+    }
+    
+    function errfn(err) {
+        test.fail();
+    }
+};
+
+exports['Run two steps flow'] = function (test) {
+    test.async();
+    var flow = simpleflow.createFlow([step1, step2], errfn);
+    
+    flow.run(1);
+    
+    function step1(value, next) {
+        test.equal(value, 1);
+        next(null, value + 1);
+    }
+    
+    function step2(value, next) {
+        test.equal(value, 2);
+        test.done();
+    }
+    
+    function errfn(err) {
+        test.fail();
+    }
 };
