ec5281f8b0bc58a9bd903645263f18a5189dbca4
ajlopez <webmaster@ajlopez.com>
2014-04-27 09:02:10 -0300

    Additional test with success for future result

--- test/sequence.js
+++ test/sequence.js
@@ -43,6 +43,26 @@ exports['execute two sync functions in sequence'] = function (test) {
     seq.run(42);
 };
 
+exports['execute two sync function in sequence with success'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data) {
+            test.equal(data, 42);
+            return data + 1;
+        },
+        function (data) {
+            test.equal(data, 43);
+            return data + 1;
+        }
+    );
+    
+    seq.run(42).success(function (data) {
+        test.equal(data, 44);
+        test.done();
+    });
+};
+
 exports['execute three sync functions in sequence'] = function (test) {
     test.async();
     
@@ -77,6 +97,20 @@ exports['execute async function in sequence'] = function (test) {
     seq.run(42);
 };
 
+exports['execute async function in sequence with success'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(function (data, next) {
+        test.equal(data, 42);
+        next(null, data + 1);
+    });
+    
+    seq.run(42).success(function (data) {
+        test.equal(data, 43);
+        test.done();
+    });
+};
+
 exports['execute two async function in sequence'] = function (test) {
     test.async();
     
@@ -93,6 +127,28 @@ exports['execute two async function in sequence'] = function (test) {
     );
     
     seq.run(42);
+};
+
+exports['execute two async function in sequence with success'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data, next) {
+            test.equal(data, 42);
+            test.ok(next);
+            next(null, data + 1);
+        },        
+        function (data, next) {
+            test.equal(data, 43);
+            test.ok(next);
+            next(null, data + 1);
+        }
+    );
+    
+    seq.run(42).success(function (data) {
+        test.equal(data, 44);
+        test.done();
+    });
 };
 
 exports['execute three async function in sequence'] = function (test) {
