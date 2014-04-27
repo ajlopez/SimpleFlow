1f44db16e65d69229251ed4f1825a4ae79a2dfc3
ajlopez <webmaster@ajlopez.com>
2014-04-27 09:35:40 -0300

    Success in sequence, with tests

--- test/sequence.js
+++ test/sequence.js
@@ -26,6 +26,20 @@ exports['execute sync function in sequence with success'] = function (test) {
     });
 };
 
+exports['execute sync function in sequence with success in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(function (data) {
+        test.equal(data, 42);
+        return data + 1;
+    }).success(function (data) {
+        test.equal(data, 43);
+        test.done();
+    });
+    
+    seq.run(42);
+};
+
 exports['execute two sync functions in sequence'] = function (test) {
     test.async();
     
@@ -63,6 +77,25 @@ exports['execute two sync function in sequence with success'] = function (test) 
     });
 };
 
+exports['execute two sync function in sequence with success in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data) {
+            test.equal(data, 42);
+            return data + 1;
+        },
+        function (data) {
+            test.equal(data, 43);
+            return data + 1;
+        }
+    ).success(function (data) {
+        test.equal(data, 44);
+        test.done();
+    });
+    
+    seq.run(42);
+};
 exports['execute three sync functions in sequence'] = function (test) {
     test.async();
     
@@ -111,6 +144,20 @@ exports['execute async function in sequence with success'] = function (test) {
     });
 };
 
+exports['execute async function in sequence with success in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(function (data, next) {
+        test.equal(data, 42);
+        next(null, data + 1);
+    }).success(function (data) {
+        test.equal(data, 43);
+        test.done();
+    });
+    
+    seq.run(42);
+};
+
 exports['execute two async function in sequence'] = function (test) {
     test.async();
     
@@ -149,6 +196,28 @@ exports['execute two async function in sequence with success'] = function (test)
         test.equal(data, 44);
         test.done();
     });
+};
+
+exports['execute two async function in sequence with success in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data, next) {
+            test.equal(data, 42);
+            test.ok(next);
+            next(null, data + 1);
+        },        
+        function (data, next) {
+            test.equal(data, 43);
+            test.ok(next);
+            next(null, data + 1);
+        }
+    ).success(function (data) {
+        test.equal(data, 44);
+        test.done();
+    });
+    
+    seq.run(42);
 };
 
 exports['execute three async function in sequence'] = function (test) {
