af8172b2d01ad52132d6aab72399c41cb5cd854b
ajlopez <webmaster@ajlopez.com>
2014-04-27 08:42:24 -0300

    Refactor step, create sequence with two functions

--- test/sequence.js
+++ test/sequence.js
@@ -2,10 +2,29 @@
 var sf = require('..');
 
 exports['execute sync function in sequence'] = function (test) {
+    test.async();
+    
     var seq = sf.sequence(function (data) {
         test.equal(data, 42);
         test.done();
     });
+    
+    seq.run(42);
+};
+
+exports['execute two sync functions in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data) {
+            test.equal(data, 42);
+            return data + 1;
+        },
+        function (data) {
+            test.equal(data, 43);
+            test.done();
+        }
+    );
     
     seq.run(42);
 };
