1acaf6421fcec8155f7759ec31ad0835215ea511
ajlopez <webmaster@ajlopez.com>
2014-04-27 08:48:50 -0300

    Three functions in sequence

--- test/sequence.js
+++ test/sequence.js
@@ -29,6 +29,27 @@ exports['execute two sync functions in sequence'] = function (test) {
     seq.run(42);
 };
 
+exports['execute three sync functions in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data) {
+            test.equal(data, 42);
+            return data + 1;
+        },
+        function (data) {
+            test.equal(data, 43);
+            return data + 1;
+        },
+        function (data) {
+            test.equal(data, 44);
+            test.done();
+        }
+    );
+    
+    seq.run(42);
+};
+
 exports['execute async function in sequence'] = function (test) {
     test.async();
     
@@ -41,3 +62,47 @@ exports['execute async function in sequence'] = function (test) {
     
     seq.run(42);
 };
+
+exports['execute two async function in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data, next) {
+            test.ok(next);
+            next(null, data + 1);
+        },        
+        function (data, next) {
+            test.ok(next);
+            test.done();
+            next(null, null);
+        }
+    );
+    
+    seq.run(42);
+};
+
+exports['execute three async function in sequence'] = function (test) {
+    test.async();
+    
+    var seq = sf.sequence(
+        function (data, next) {
+            test.equal(data, 42);
+            test.ok(next);
+            next(null, data + 1);
+        },        
+        function (data, next) {
+            test.equal(data, 43);
+            test.ok(next);
+            next(null, data + 1);
+        },        
+        function (data, next) {
+            test.equal(data, 44);
+            test.ok(next);
+            test.done();
+            next(null, null);
+        }
+    );
+    
+    seq.run(42);
+};
+
